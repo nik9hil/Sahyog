79ffb0258fa76d0ba84c2623666150bd4209ff38
vidyam99 <vidyajmakwana@gmail.com>
2020-01-29 12:15:25 +0530

    changes

--- SAHYOG/authentication/views.py
+++ SAHYOG/authentication/views.py
@@ -1,6 +1,4 @@
-#Import Dependencies
 from django.shortcuts import render,redirect
-from django.contrib.auth.decorators import login_required
 import pyrebase
 from django.contrib import auth
 
@@ -17,20 +15,99 @@ config = {
 firebase = pyrebase.initialize_app(config)
 authenticate = firebase.auth()
 database = firebase.database()
+def login(request):
+  return render(request,'authentication/login.html')
 
-# Create your views here.
-
-def home(request):
+def sahyog(request):
+  email = request.POST.get('email')
+  password = request.POST.get('password')
   try:
-    a = authenticate.get_account_info(request.session['uid'])
-    a = a['users'][0]['email']
-    context = {
-      'email' : a
-    }
-    return render(request,'home/home.html',context)
+    user = authenticate.sign_in_with_email_and_password(email,password)
   except:
-    message = "Oops! User logged out. Please log in again."
+    try:
+      if request.session['uid']:
+        email = request.session['email']
+        context = {
+          'email' : email
+        }
+        return render(request,'home/home.html',context)
+    except:
+      message = "You are logged out. Log in again."
+      context = {
+        'message' : message
+      }
+      return render(request,'authentication/login.html',context)
+    message = "Invalid Credentials. Please try again."
     context = {
-      "message" : message
+      'message' : message
     }
-    return render(request,'authentication/login.html',context)
+    return render(request,"authentication/login.html",context)
+  session_id = user['idToken']
+  request.session['uid'] = str(session_id)
+  request.session['email'] = str(user['email'])
+  message = "You are logged in"
+  context = {
+    'email' : email,
+    'message' : message
+  }
+  return render(request,'home/home.html',context)
+
+def logout(request):
+  try:
+    del request.session['uid']
+  except KeyError:
+    pass
+  return render(request,'authentication/login.html')
+
+def signup(request):
+  return render(request,'authentication/signup.html')
+
+def postsignup(request):
+  name = request.POST.get('name')
+  regnumber = request.POST.get('regnumber')
+  organisation = request.POST.get('organisation')
+  service = request.POST.get('service')
+  donation = request.POST.get('donation')
+  atg_certi = request.POST.get('atg_certi')
+  donation_link = request.POST.get('donation_link')
+  adoption = request.POST.get('adoption')
+  adoption_capacity = request.POST.get('adoption_capacity')
+  total_adopted = request.POST.get('total_adopted')
+  rescue = request.POST.get('rescue')
+  total_rescued = request.POST.get('total_rescued')
+  treatment = request.POST.get('treatment')
+  email = request.POST.get('email')
+  password = request.POST.get('password')
+  try:
+    user = authenticate.create_user_with_email_and_password(email,password)
+  except:
+    message = "Unable to create account. Try again."
+    context = {
+    'message' : message
+    }
+    return render(request,'authentication/signup.html',context)
+  uid = user['localId']
+  idtoken = user['idToken']
+  message = "Account created successfully."
+  success = True
+  data = {
+    'name':name,
+    'email':email,
+    'regnumber' : regnumber,
+    'organisation' : organisation,
+    'service' : service,
+    'donation' : donation,
+    'atg_certi' : atg_certi,
+    'donation_link' : donation_link,
+    'adoption' : adoption,
+    'adoption_capacity' : adoption_capacity,
+    'total_adopted' : total_adopted,
+    'total_rescued' : total_rescued,
+    'treatment' : treatment
+  }
+  context = {
+    'message' : message,
+    'success' : success
+  }
+  database.child('users').child(uid).child('details').set(data,idtoken)
+  return render(request,'authentication/signup.html',context)
