7e73228fd42cdefc7b74ab3062adb7f90bd71420
Vidya Makwana <46899951+vidyam99@users.noreply.github.com>
2020-01-30 06:56:48 +0530

    Add files via upload

--- SAHYOG/report/views.py
+++ SAHYOG/report/views.py
@@ -25,53 +25,9 @@ database = firebase.database()
 
 class HomePageView(View):
     def get(self, request, *args, **kwargs):
-        idtoken = request.session['uid']
-        a = authenticate.get_account_info(idtoken)
-        a = a['users']
-        a = a[0]
-        mailid = a['email']
-        a = a['localId']
-        allusers = database.child('users').shallow().get().val()
-        complaint_list=[]
-        #print(allusers)
-        if allusers != None:
-            users = []
-            for i in allusers:
-                if i != a:
-                    users.append(i)
-            user_complaints = []
-            complaintName = []
-            animal = []
-            address = []
-            description = []
-            for i in users:
-                allusercomplaints = database.child('users').child(i).child('complaints').shallow().get().val()
-                if allusercomplaints != None:
-                    user_complaints.append(allusercomplaints)
-                    for j in user_complaints[-1]:
-                        print(j)
-                        ename = database.child('users').child(i).child('complaints').child(j).child('complaint').get().val()
-                        dat = database.child('users').child(i).child('complaints').child(j).child('animal').get().val()
-                        addr = database.child('users').child(i).child('complaints').child(j).child('address').get().val()
-                        descr = database.child('users').child(i).child('complaints').child(j).child('description').get().val()
-                        complaintName.append(ename)
-                        animal.append(dat)
-                        address.append(addr)
-                        description.append(descr)
-                else:
-                    continue
-        
-            complaint_list = zip(complaintName,animal,address,description)
-        context = {
-            'email' : mailid,
-            'complaint_list' : complaint_list
-        }
-        return render(request,'report/reports.html',context)
+        return render(request,'report/reports.html')
 
 class ChartData(APIView):
-    authentication_classes = []
-    permission_classes = []
-
     def get(self, request, format=None):
         idtoken = request.session['uid']
         a = authenticate.get_account_info(idtoken)
@@ -79,23 +35,30 @@ class ChartData(APIView):
         a = a[0]
         mailid = a['email']
         a = a['localId']
-        data={"Dadar":0,"Byculla":0,"Kalyan":0,"Vidyavihar":0,"Ghatkopar":0,"Kurla":0,"Thane":0,"Vikhroli":0,"Andheri":0}
         allusers = database.child('users').shallow().get().val()
         if allusers != None:
             users = []
             for i in allusers:
                 if i != a:
                     users.append(i)
+        data={}
+        labels=[]
         for i in users:
-            auser = database.child('users').child(i).child('details').child('organisation').shallow().get().val()
-            if auser!= None and auser=="NGO":
-                fuser = database.child('users').child(i).child('details').child('service').shallow().get().val()
-                if fuser!=None:
-                    data[fuser]+=1
-
+            auser = database.child('users').child(i).child('details').child('service').get().val()
+            auser = auser.lower()
+            if auser in data.keys():
+                data[auser]+=1
+            else:
+                data[auser]=1
+                labels.append(auser)
+        print(labels)
+        print(data)
+        datatemp=[]
+        for key in data.keys():
+            datatemp.append(data[key])
         data = {
-            "labels": ["Kalyan","Kurla", "Vidyavihar","Ghatkopar","Vikhroli","Byculla","Dadar","Thane","Andheri"],
-            "data": [data["Kalyan"], data["Kurla"], data["Vidyavihar"], data["Ghatkopar"], data["Vikhroli"], data["Byculla"], data["Dadar"],data["Thane"],data["Andheri"]],
+            "labels": labels,
+            "data": datatemp,
         }
         return Response(data)
 
